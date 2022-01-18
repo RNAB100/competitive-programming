932f0260d995ed0f378abdacb536a17e1f669ea0
Arnab Singha <73381363+RNAB100@users.noreply.github.com>
2022-01-19 01:13:04 +0600

    Add files via upload
    O(NLogN)

--- Prims_algorithm.cpp
+++ Prims_algorithm.cpp
@@ -16,16 +16,14 @@ typedef   long long int  ll;
 typedef   vector<ll>     vll;
 
 //constant
-#define Pi    3.141592653589793
-#define mod   998244353//1000000007
-#define N     100005
-#define INF   2147483647 
+#define Pi     3.141592653589793
+#define mod    998244353//1000000007
+#define N      100005
+#define INF    2147483647 
 // 
-#define fastio 		ios_base::sync_with_stdio(false); cin.tie(NULL) 
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL) 
 
 int main() {
-
-	//fastio;
 	ll i, j, n, m;
 	cin >> n >> m;
 	vector<pair<ll, ll>> adj[n];
@@ -40,20 +38,19 @@ int main() {
 	ll parent[n];
 	bool mst[n];
 	ll key[n];
+	//pq --> key value ,idx;
+	//minimum priority queue or min heap
+	priority_queue< pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll,ll>> >pq;
 	for(i = 0; i < n; i++) {
 		key[i] = INF;
 		mst[i] = false;	
 	}
 	key[0] = 0; //initialize smallest non-existing node of mst 0
 	parent[0] = -1;
-	for(i = 0; i < n-1; i++) { //n-1 nodes of spanning tree
-		ll u, mini = INF;
-		for(ll v = 0; v < n; v++) {
-			if(mst[v] == false && key[v] < mini) {
-				mini = key[v];
-				u = v;
-			}
-		}
+	pq.push({0,0});
+	while(pq.empty() == 0) { //n-1 nodes of spanning tree
+		ll u = pq.top().ss; //getting u in logn
+		pq.pop();
 		mst[u] = true; //put u node on mst
 		for(auto it : adj[u]) { //all adj nodes of u
 			ll v = it.ff;
@@ -62,6 +59,7 @@ int main() {
 			if(mst[v] == false && weight < key[v]) { 
 				parent[v] = u;
 				key[v] = weight;
+				pq.push({key[v],v});
 			}			
 
 		}
@@ -72,3 +70,4 @@ int main() {
 	}
 	return 0;
 }
+ 
